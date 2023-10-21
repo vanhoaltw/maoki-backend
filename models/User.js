25e176bb4fced49695c52039a205fc6f2d0f994c
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-21 16:05:14 +0600

    User model created

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        // Regular expression for basic email validation
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+      },
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photoURL: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: [18, "Age must be at least 18 years"],
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+    required: true,
+  },
+  nid: {
+    type: String, // Assuming National ID is alphanumeric, use String
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["GENERAL", "MODERATE", "ADMIN"],
+    default: "GENERAL",
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
