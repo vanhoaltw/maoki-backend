80091c1d7041d5374601004761cde088892f0161
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-25 07:55:46 +0600

    manger controller

---
+++ controllers/managerController.js
@@ -0,0 +1,105 @@
+const Hotel = require("../models/Hotel");
+const isFieldsRequired = require("../utils/isFieldsRequired");
+const throwError = require("../utils/throwError");
+
+const getHotel = async (req, res, next) => {
+  try {
+    const result = await Hotel.findOne({managerId: req.user._id});
+
+    if (!result) throwError("Couldn't find hotel", 404);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postHotel = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const isRequired = isFieldsRequired(data, [
+      "name",
+      "photoURL",
+      "description",
+      "availableRoom",
+      "address",
+    ]);
+
+    if (!isRequired) {
+      throwError("All fields is required", 404);
+    }
+
+    data.managerId = req.user._id;
+
+    const existingHotel = await Hotel.findOne({managerId: req.user._id});
+
+    if (existingHotel) {
+      throwError("Hotel already exists", 409);
+    }
+
+    const newHotel = new Hotel(data);
+
+    const result = await newHotel.save();
+
+    res.json({message: "Hotel saved successfully", hotel: result});
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateHotel = async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    if (data.length <= 0) throwError("Please provide a updated hotel", 404);
+
+    const existingHotel = await Hotel.findOne({managerId: req.user._id});
+
+    if (!existingHotel) {
+      throwError("Hotel is not found", 404);
+    }
+
+    const updatedHotel = await Hotel.findOneAndUpdate(
+      {_id: existingHotel._id, managerId: req.user._id},
+      data,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedHotel) throwError("User not found", 404);
+
+    res.json({
+      message: "Hotel updated successfully",
+      updatedHotel,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const deleteHotel = async (req, res, next) => {
+  try {
+    const existingHotel = await Hotel.findOne({managerId: req.user._id});
+
+    if (!existingHotel) {
+      throwError("Couldn't find hotel", 404);
+    }
+
+    const deleteHotel = await Hotel.deleteOne({
+      _id: existingHotel._id,
+      managerId: req.user._id,
+    });
+
+    if (!deleteHotel.deletedCount) {
+      throwError("Couldn't deleted Hotel", 404);
+    }
+
+    res.json({
+      message: "Hotel delete successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {getHotel, postHotel, updateHotel, deleteHotel};
--- routes/manager.js
+++ routes/manager.js
@@ -1,65 +1,13 @@
-const Hotel = require("../models/Hotel");
-const isFieldsRequired = require("../utils/isFieldsRequired");
-const throwError = require("../utils/throwError");
-
 const router = require("express").Router();
+const mangerController = require("../controllers/managerController");
 
-router.get("/hotel", async (req, res, next) => {
-  try {
-    const result = await Hotel.findOne();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/hotel", mangerController.getHotel);
 
-router.post("/hotel", async (req, res, next) => {
-  try {
-    const data = req.body;
-    const isRequired = isFieldsRequired(data, [
-      "name",
-      "photoURL",
-      "description",
-      "availableRoom",
-      "address",
-    ]);
+router.post("/hotel", mangerController.postHotel);
 
-    if (!isRequired) {
-      throwError("All fields is required", 404);
-    }
+router.put("/hotel", mangerController.updateHotel);
 
-    data.managerId = req.user._id;
-
-    const existingHotel = await Hotel.findOne();
-
-    if (existingHotel) {
-      throwError("Hotel already exists", 404);
-    }
-
-    const newHotel = new Hotel(data);
-
-    const result = await newHotel.save();
-
-    res.json({message: "Hotel saved successfully", hotel: result});
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/hotel", (req, res, next) => {
-  try {
-    const data = req.body;
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/hotel", (req, res, next) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/hotel", mangerController.deleteHotel);
 
 router.get("/", async (req, res, next) => {
   try {
