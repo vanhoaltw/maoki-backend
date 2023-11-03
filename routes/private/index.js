b885ce6f8e379f8bb9f170c697e538b5af43fc3f
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-03 23:56:43 +0600

    hotel details change to private to public

--- routes/private/index.js
+++ routes/private/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
-const hotelDetailsRoutes = require("./hotel-details");
 const bookingRoutes = require("./booking");
 const blogRoutes = require("./blog");
 
 router.use("/user", userRoutes);
-router.use("/hotel", hotelDetailsRoutes);
 router.use("/booking", bookingRoutes);
 router.use("/blog", blogRoutes);
 
--- routes/public/hotel.js
+++ routes/public/hotel.js
@@ -1,7 +1,24 @@
+const router = require("express").Router();
 const Hotel = require("../../models/Hotel");
+const Room = require("../../models/Room");
 const throwError = require("../../utils/throwError");
 
-const router = require("express").Router();
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    if (!id) throwError("Invalid id");
+
+    const hotel = await Hotel.findById(id);
+
+    if (!hotel) throwError("No hotel found");
+
+    const rooms = await Room.find({hotelId: hotel._id});
+
+    res.json({hotel, rooms});
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/", async (req, res, next) => {
   try {
