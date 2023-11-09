b119ff3c6596887624078aec63db23dd81a14acc
AbuBokorprog <abubokor1066@gmail.com>
2023-11-09 11:10:26 +0600

    get location id, name, thumbnail and total hotel

--- routes/public/locations.js
+++ routes/public/locations.js
@@ -5,16 +5,28 @@ const router = require("express").Router();
 router.get("/", async (req, res, next) => {
   try {
     let location = await Hotel.find();
+    const locationCounts = {};
 
-    location = location.map((singleLocation) => {
-      return {
-        _id: singleLocation._id,
-        name: singleLocation.address.location,
-        thumbnail: singleLocation.address.thumbnailURL,
-      };
+    location.forEach((hotel) => {
+      const locationId = hotel._id;
+      const name = hotel.address.location;
+      const thumbnailURL = hotel.address.thumbnailURL;
+
+      if (locationCounts[name]) {
+        locationCounts[name].total_hotel++;
+      } else {
+        locationCounts[name] = {
+          _id: locationId,
+          name: name,
+          ThumbnailURL: thumbnailURL,
+          total_hotel: 1,
+        };
+      }
     });
 
-    res.json(location);
+    // Convert the locationCounts object into an array of objects for easier processing
+    const locationData = Object.values(locationCounts);
+    res.json(locationData);
   } catch (error) {
     next(error);
   }
