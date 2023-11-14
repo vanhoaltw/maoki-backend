d349078f7ffe08c5d59b73910fabe60b0ec376c2
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-14 18:54:05 +0600

    find rooms using room ids

--- routes/private/room.js
+++ routes/private/room.js
@@ -4,6 +4,21 @@ const throwError = require("../../utils/throwError");
 
 const router = require("express").Router();
 
+router.get("/roomIds", async (req, res, next) => {
+  try {
+    const roomIds = req.query.roomIds;
+    if (roomIds.length == 0) throwError("give me roomIds []", 404);
+
+    const rooms = await Room.find({_id: {$in: roomIds}});
+
+    if (!rooms) throwError("Room not found", 404);
+
+    res.json(rooms);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get("/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
