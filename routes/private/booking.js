2f2e5465df0d86a9ab213a2c79f3f2c69b13e2e6
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-15 11:50:54 +0600

    add hotel name and id

--- routes/private/booking.js
+++ routes/private/booking.js
@@ -1,15 +1,31 @@
+const Hotel = require("../../models/Hotel");
 const Payment = require("../../models/Payment");
+const Room = require("../../models/Room");
 const throwError = require("../../utils/throwError");
 
 const router = require("express").Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const bookingDetails = await Payment.find({email: req.user.email});
+    let bookingDetails = await Payment.find({email: req.user.email});
 
-    if (!bookingDetails) {
-      throwError("Booking details not found");
+    if (bookingDetails.length === 0) {
+      throw new Error("Booking details not found");
     }
+
+    bookingDetails = await Promise.all(
+      bookingDetails.map(async (booking) => {
+        const roomId = booking.rooms[0].roomId;
+        const room = await Room.findById(roomId);
+        const hotel = await Hotel.findById(room.hotelId);
+        return {
+          ...booking.toObject(),
+          hotelName: hotel.name,
+          hotelId: hotel._id,
+        };
+      })
+    );
+
     res.json(bookingDetails);
   } catch (error) {
     next(error);
