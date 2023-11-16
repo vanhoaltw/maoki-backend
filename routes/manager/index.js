3f94a02022245031e326aeb808c48ecb4c937e91
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-16 18:13:23 +0600

    manager can see more details

--- routes/manager/index.js
+++ routes/manager/index.js
@@ -5,6 +5,7 @@ const throwError = require("../../utils/throwError");
 const hotelRoutes = require("./hotel");
 const roomRoutes = require("./room");
 const bookingHistoryRoutes = require("./booking-history");
+const Payment = require("../../models/Payment");
 
 router.use("/room", roomRoutes);
 router.use("/hotel", hotelRoutes);
@@ -17,7 +18,38 @@ router.get("/", async (req, res, next) => {
 
     const rooms = await Room.countDocuments({hotelId: hotel._id});
 
-    res.json({rooms, currentBooking: 0, totalBooking: 0, totalCustomer: 0});
+    const totalCustomer = await Payment.distinct("email", {hotelId: hotel._id});
+
+    const totalBooking = await Payment.countDocuments({hotelId: hotel._id});
+
+    const currentDate = new Date(); // Current date
+
+    const currentBookings = await Payment.find({
+      hotelId: hotel._id,
+      rooms: {
+        $elemMatch: {
+          checkIn: {$lte: currentDate},
+          checkOut: {$gte: currentDate},
+        },
+      },
+    });
+
+    const pendingBooking = await Payment.find({
+      hotelId: hotel._id,
+      rooms: {
+        $elemMatch: {
+          checkIn: {$gte: currentDate},
+        },
+      },
+    });
+
+    res.json({
+      rooms,
+      currentBooking: currentBookings.length,
+      pendingBooking: pendingBooking.length,
+      totalBooking: totalBooking,
+      totalCustomer: totalCustomer.length,
+    });
   } catch (error) {
     next(error);
   }
