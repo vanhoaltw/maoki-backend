181a2717e0fccd94d05d1ee6135d24103b509049
hoanguyen <vanhoaltw@gmail.com>
2023-12-18 12:58:45 +0700

    remove file filter

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,24 +1,24 @@
 const multer = require("multer");
-const path = require("path");
+// const path = require("path");
 
 const uploadImage = multer({
-  fileFilter: (req, file, cb) => {
-    const fileExts = [".png", ".jpg", ".jpeg", ".gif"];
+  // fileFilter: (req, file, cb) => {
+  //   const fileExts = [".png", ".jpg", ".jpeg", ".gif"];
 
-    // Check allowed extensions
-    const isAllowedExt = fileExts.includes(
-      path.extname(file.originalname.toLowerCase())
-    );
+  //   // Check allowed extensions
+  //   const isAllowedExt = fileExts.includes(
+  //     path.extname(file.originalname.toLowerCase())
+  //   );
 
-    // Mime type must be an image
-    const isAllowedMimeType = file.mimetype.startsWith("image/");
+  //   // Mime type must be an image
+  //   const isAllowedMimeType = file.mimetype.startsWith("image/");
 
-    if (isAllowedExt && isAllowedMimeType) {
-      return cb(null, true); // no errors
-    }
-    // pass error msg to callback, which can be displaye in frontend
-    return cb("Error: File type not allowed!");
-  },
+  //   if (isAllowedExt && isAllowedMimeType) {
+  //     return cb(null, true); // no errors
+  //   }
+  //   // pass error msg to callback, which can be displaye in frontend
+  //   return cb("Error: File type not allowed!");
+  // },
 
   limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
 });
